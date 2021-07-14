58d57ef15709051031720786a71ac0361660113b
Simon Ledoux <simon@simon511000.fr>
2021-07-14 22:03:28 +0200

    add identifier and logger

--- src/main/java/eu/octanne/codelyoko/CodeLyokoMain.java
+++ src/main/java/eu/octanne/codelyoko/CodeLyokoMain.java
@@ -1,14 +1,30 @@
 package eu.octanne.codelyoko;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 
 public class CodeLyokoMain implements ModInitializer {
+
+	public static Logger LOGGER = LogManager.getLogger();
+
+	public static final String MOD_ID = "codelyoko";
+    public static final String MOD_NAME = "CodeLyoko";
+    public static final Identifier MOD_FULL_ID = new Identifier(CodeLyokoMain.MOD_ID, CodeLyokoMain.MOD_ID);
+
+	public static void log(Level level, String message) {
+        LOGGER.log(level, "[" + MOD_NAME + "] " + message);
+    }
+
+	public static void log(String message) {
+        LOGGER.log(Level.INFO, "[" + MOD_NAME + "] " + message);
+    }
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		CodeLyokoMain.log("Initialisation du mod CodeLyoko!");
 	}
 }
